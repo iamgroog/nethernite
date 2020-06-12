d9f640eccf2ab8d840484e21899fca38effd78f1
Daniel Popov <galanest@ya.ru>
2020-06-12 20:14:04 +0300

    Added store methods

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,31 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
+import constructSearchRequestToNPM from "./../script/constructSearchRequestToNPM";
+import sendRequest from "./../script/sendRequest";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    packages: {},
+    query: "",
+    totalPackagesFound: null
   },
   getters: {
   },
   mutations: {
+    SET_QUERY(state, query){
+      state.query = query
+    }
   },
   actions: {
-  },
-  modules: {
+    searchPackagesInNPM({ commit }, query){
+      commit("SET_QUERY", query);
+    },
+    searchPackages({ commit }, query){
+      commit("SET_QUERY", query);
+      const request = constructSearchRequestToNPM(query);
+      sendRequest(request);
+    }
   }
 })
