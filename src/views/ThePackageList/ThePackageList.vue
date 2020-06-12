6d2e3441ed9a7493d6ed476fdde88c9a20fd3fcc
Daniel Popov <galanest@ya.ru>
2020-06-12 10:06:59 +0300

    Added package list component template

---
+++ src/views/ThePackageList/ThePackageList.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+
+  div.the-package-list the-package-list
+
+</template>
+
+<script>
+
+export default {
+  name: 'the-package-list',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+  .the-package-list{
+    position: relative;
+
+    height: 100%;
+    width: 100%;
+  }
+
+</style>
